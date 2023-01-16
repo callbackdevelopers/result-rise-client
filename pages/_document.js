86efc56d8337e99fbfc7e8da9d119fe6c1fbf3cf
itzshakeeb <itzshakeeb@gmail.com>
2023-01-16 20:30:16 +0600

    added custom fonts

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,14 @@
-<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,700&display=swap" rel="stylesheet"></link>
+
 
 import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
