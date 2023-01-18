483cf029b846809bb255db59bde675e3a8d25292
itzshakeeb <itzshakeeb@gmail.com>
2023-01-18 23:26:00 +0600

    modify body template

--- components/Navbars/Sidebar.js
+++ components/Navbars/Sidebar.js
@@ -1,13 +1,11 @@
-import React, { useMemo, useState } from 'react';
-
+import classNames from "classnames";
+import React, { useState } from 'react';
 import { AiOutlineFileDone } from 'react-icons/ai';
 import { BiBookOpen, BiNews } from 'react-icons/bi';
+import { FaUserTie } from 'react-icons/fa';
 import { FiSettings, FiUsers } from 'react-icons/fi';
 import { ImNotification } from 'react-icons/im';
 import { MdDashboardCustomize } from 'react-icons/md';
-
-import classNames from "classnames";
-import { useRouter } from 'next/router';
 import { CollapseIcon, LogoIcon, LogoutIcon } from '../icons';
 import LinkTemplate from '../Shared/NavbarLinkTemplate/LinkTemplate';
 
@@ -16,23 +14,15 @@ const menuItems = [
     { id: 1, label: "Dashboard", icon: <MdDashboardCustomize />, link: "/admin/dashboard" },
     { id: 2, label: "Student Classes", icon: <BiNews />, link: "/admin/studentClasses" },
     { id: 3, label: "Subject", icon: <BiBookOpen />, link: "/admin/subject" },
-    { id: 4, label: "Student", icon: <FiUsers />, link: "/admin/student" },
+    // { id: 4, label: "Student", icon: <FiUsers />, link: "/admin/student" },
     { id: 5, label: "Result", icon: <AiOutlineFileDone />, link: "/admin/result" },
     { id: 6, label: "Notice", icon: <ImNotification />, link: "/admin/notice" },
     { id: 7, label: "Admin Settings", icon: <FiSettings />, link: "/admin/adminSettings" },
 ];
 
 const Sidebar = () => {
-    const [toggleCollapse, setToggleCollapse] = useState(false)
-    const [isCollapsed, setIsCollapsed] = useState(false)
-
-
-    const router = useRouter();
-
-    const activeMenu = useMemo(
-        () => menuItems.find((menu) => menu.link === router.pathname),
-        [router.pathname]
-    );
+    const [toggleCollapse, setToggleCollapse] = useState(false);
+    const [isCollapsed, setIsCollapsed] = useState(false);
 
     const wrapperClasses = classNames(
         "h-screen px-4 pt-8 pb-4 bg-light flex justify-between flex-col",
@@ -41,33 +31,21 @@ const Sidebar = () => {
             ["w-20"]: toggleCollapse,
         }
     );
-
-    const CollapseIconClasses = classNames('p-4 rounded bg-gray-200 text-slate-400 absolute right-0',
+    const CollapseIconClasses = classNames('p-4 rounded bg-gray-200 text-slate-400 absolute right-0 ',
         {
             "rotate-180": toggleCollapse,
         })
-
-    const getNavItemClasses = (menu) => {
-        return classNames(
-            "flex items-center cursor-pointer hover:bg-gray-300 rounded w-full overflow-hidden whitespace-nowrap",
-            {
-                ["bg-bg-gray-400"]: activeMenu?.id === menu.id,
-            }
-        );
-    };
-
-
     const onMouseOver = () => {
         setIsCollapsed(!isCollapsed);
     };
     const handleSidebarToggle = () => {
         setToggleCollapse(!toggleCollapse);
     };
-
     return (
-
-        <div className='shadow-xl'>
-            <div className={wrapperClasses} onMouseEnter={onMouseOver} onMouseLeave={onMouseOver}
+        <div className='shadow-xl '>
+            <div className={wrapperClasses}
+                onMouseEnter={onMouseOver}
+                onMouseLeave={onMouseOver}
                 style={{ transition: "width 300ms cubic-bezier(0.2, 0, 0, 1) 0s" }}
             >
                 <div className='flex flex-col'>
@@ -79,21 +57,33 @@ const Sidebar = () => {
                             </span>
                         </div>
                         {isCollapsed &&
-                            <button className={CollapseIconClasses} onClick={handleSidebarToggle}>
-                                <CollapseIcon></CollapseIcon>
+                            <button className={`${CollapseIconClasses}`} onClick={handleSidebarToggle}>
+                                <CollapseIcon />
                             </button>
                         }
                     </div>
                 </div>
-                <div className="flex flex-col items-start mt-20">
+                <div className="flex flex-col items-start mt-10">
                     <LinkTemplate
-                        icon={<FiSettings />}
-                        title={"dd"}
+                        icon={<BiNews />}
+                        title={"Profile"}
                         link={'/dashboard/profile'}
                         toggleCollapse={toggleCollapse}
                     />
+                    <LinkTemplate
+                        icon={<FiUsers />}
+                        title={"Studets"}
+                        link={'/dashboard/students'}
+                        toggleCollapse={toggleCollapse}
+                    />
+                    <LinkTemplate
+                        icon={<FaUserTie />}
+                        title={"Teachers"}
+                        link={'/dashboard/teachers'}
+                        toggleCollapse={toggleCollapse}
+                    />
                 </div>
-                <div className={`${getNavItemClasses({})} px-4 py-4`}>
+                <div className='px-4 py-4 flex'>
                     <div style={{ width: "1.5rem" }}>
                         <LogoutIcon />
                     </div>
