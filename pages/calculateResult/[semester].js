560e0524cb5d0c38228149adf8bd6bc5a5ca5cd4
FFajibraj779r <mdrajikuli757@gmail.com>
2023-01-19 22:58:27 +0600

    calucalte result

---
+++ pages/calculateResult/[semester].js
@@ -0,0 +1,17 @@
+
+
+const semester = () => {
+
+    
+
+
+    return (
+        <div>
+            <h1>semester all here </h1>
+            
+        </div>
+    );
+};
+
+export default semester;
+
