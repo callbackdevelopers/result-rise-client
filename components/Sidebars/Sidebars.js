f21b96783527cbc69fd53983b6ebac4f3ff8db84
itzshakeeb <itzshakeeb@gmail.com>
2023-01-22 20:22:31 +0600

    added some link

--- components/Sidebars/Sidebars.js
+++ components/Sidebars/Sidebars.js
@@ -1,10 +1,12 @@
+import Link from "next/link";
+import React from 'react';
 const Sidebars = () => {
     return (
         <div className="drawer-side">
             <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
             <ul className="menu p-4 w-80 bg-base-100 lg:bg-inherit text-base-content">
-                <li><a>Sidebar Item 1</a></li>
-                <li><a>Sidebar Item 2</a></li>
+                <li><a><Link href="/dashboard/profile">Profile</Link></a></li>
+                <li><a><Link href="/dashboard/students">Students</Link></a></li>
             </ul>
         </div>
     );
