acf98737de7ac30c1b7204ea3f2a00bd054266db
FFajibraj779r <mdrajikuli757@gmail.com>
2023-02-03 00:21:30 +0600

    semester result

--- pages/dashboard/students/pending/index.js
+++ pages/dashboard/students/pending/index.js
@@ -1,42 +1,17 @@
-import { useQuery } from "@tanstack/react-query";
-import DashboardNavbar from "../../../../components/Navbars/DashboardNavbar";
-import Pending from "../../../../components/Shared/Pending/pending";
-import Sidebars from "../../../../components/Sidebars/Sidebars";
-import Spiner from "../../../../components/Spiner/Spiner";
-import AlertMessage from "../../../../Hooks/AlertMessage";
-import { useQuery } from "@tanstack/react-query";
-import DashboardNavbar from "../../../../components/Navbars/DashboardNavbar";
-import UsersTableTamplete from "../../../../components/Shared/UsersTableTamplete/usersTableTamplete";
-import Sidebars from '../../../../components/Sidebars/Sidebars';
-import Spiner from "../../../../components/Spiner/Spiner";
-import AlertMessage from "../../../../Hooks/AlertMessage";
+import { useQuery } from "@tanstack/react-query"
+import DashboardNavbar from "../../../../components/Navbars/DashboardNavbar"
+import Pending from "../../../../components/Shared/Pending/pending"
+import Sidebars from "../../../../components/Sidebars/Sidebars"
+import Spiner from "../../../../components/Spiner/Spiner"
+import AlertMessage from "../../../../Hooks/AlertMessage"
 
 
 
 
 const index = () => {
-  const { successMessage } = AlertMessage();
-  const url = `http://localhost:3100/users`;
-  const {
-    data: students = [],
-    refetch,
-    isLoading,
-  } = useQuery({
-    queryKey: [],
-    queryFn: async () => {
-      const res = await fetch(url);
-      const data1 = await res.json();
-      const data = data1.filter(
-        (ps) => !ps.verification && ps.roll === "student"
-      );
-      return data;
-    },
-  });
-  console.log("Stu", students);
     const { successMessage } = AlertMessage();
-    const btnName = "Approve";
     const url = `http://localhost:3100/users`
-    const { data: users = [], refetch, isLoading } = useQuery({
+    const { data: students = [], refetch, isLoading } = useQuery({
         queryKey: [],
         queryFn: async () => {
             const res = await fetch(url)
@@ -45,79 +20,26 @@ const index = () => {
             return data;
         }
     })
-    console.log("UsersTableTamplete: ", users)
+    console.log("Stu", students)
 
-  if (isLoading) {
-    return <Spiner></Spiner>;
-  }
+    if (isLoading) {
+        return <Spiner></Spiner>
+    }
 
-  const handleVerification = (student) => {
-    console.log("Deleting  with id: ", student);
+    const handleVerification = (student) => {
+        console.log('Deleting  with id: ', student)
 
-    fetch(`http://localhost:3100/users/${student._id}`, {
-      method: "PATCH",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(),
-    })
-      .then((res) => res.json())
-      .then((result) => {
-        console.log("resultInside", result);
-        if (result.acknowledged === true) {
-          successMessage("Approved");
-          refetch();
-        }
-      });
-  };
-  return (
-    <>
-      <DashboardNavbar />
-      <div className="drawer drawer-mobile">
-        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content ">
-          <div className="overflow-x-auto w-full">
-            <table className="table w-full">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Address</th>
-                  <th>Depertment</th>
-                  <th></th>
-                </tr>
-              </thead>
-              {students?.map((student) => (
-                <Pending
-                  id={student._id}
-                  student={student}
-                  handleVerification={handleVerification}
-                ></Pending>
-              ))}
-              <tfoot>
-                <tr>
-                  <th></th>
-                  <th></th>
-                  <th></th>
-                  <th></th>
-                </tr>
-              </tfoot>
-            </table>
-          </div>
-        </div>
-        <Sidebars></Sidebars>
-      </div>
-    </>
-  );
-};
-    const handleUser = (user) => {
-        console.log('Verification  with id: ', user)
-        fetch(`http://localhost:3100/users/${user._id}`, {
-            method: 'PATCH'
+        fetch(`http://localhost:3100/users/${student._id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify()
         })
             .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                if (data.acknowledged) {
+            .then(result => {
+                console.log("resultInside", result);
+                if (result.acknowledged === true) {
                     successMessage("Approved")
                     refetch()
                 }
@@ -141,12 +63,11 @@ const index = () => {
                                 </tr>
                             </thead>
                             {
-                                users?.map(user => <UsersTableTamplete
-                                    id={user._id}
-                                    user={user}
-                                    handleUser={handleUser}
-                                    btnName={btnName}
-                                ></UsersTableTamplete>)
+                                students?.map(student => <Pending
+                                    id={student._id}
+                                    student={student}
+                                    handleVerification={handleVerification}
+                                ></Pending>)
                             }
                             <tfoot>
                                 <tr>
@@ -166,3 +87,6 @@ const index = () => {
 }
 
 export default index;
+
+
+
