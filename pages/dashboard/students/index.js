6fed01c832e52da08134558accfbb92e158552e2
FFajibraj779r <mdrajikuli757@gmail.com>
2023-02-02 21:37:06 +0600

    get data s

--- pages/dashboard/students/index.js
+++ pages/dashboard/students/index.js
@@ -4,79 +4,78 @@ import Sidebars from "../../../components/Sidebars/Sidebars";
 import Spiner from "../../../components/Spiner/Spiner";
 import Student from "./student";
 
-
 const index = () => {
-    const url = `http://localhost:3100/users`
-    const { data: students = [], refetch, isLoading } = useQuery({
-        queryKey: [],
-        queryFn: async () => {
-            const res = await fetch(url)
-            const data1 = await res.json()
-            const data = data1.filter(u => u.roll === "student")
-            return data;
-        }
+  const url = `http://localhost:3100/users`;
+  const {
+    data: students = [],
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: [],
+    queryFn: async () => {
+      const res = await fetch(url);
+      const data1 = await res.json();
+      const data = data1.filter((u) => u.roll === "student");
+      return data;
+    },
+  });
+  console.log("Stu", students);
+
+  if (isLoading) {
+    return <Spiner></Spiner>;
+  }
+
+  const handleStudentDelete = (student) => {
+    console.log("Deleting  with id: ", student);
+    fetch(`http://localhost:3100/users/${student._id}`, {
+      method: "DELETE",
     })
-    console.log("Stu", students)
-
-    if (isLoading) {
-        return <Spiner></Spiner>
-    }
-
-    const handleStudentDelete = (student) => {
-        console.log('Deleting  with id: ', student)
-        fetch(`http://localhost:3100/users/${student._id}`, {
-            method: 'DELETE'
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                if (data.deletedCount > 0) {
-                    refetch();
-                }
-            })
-
-    }
-    return (
-        <>
-            <DashboardNavbar />
-            <div className="drawer drawer-mobile">
-                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content ">
-                    <div className="overflow-x-auto w-full">
-                        <table className="table w-full">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Address</th>
-                                    <th>Depertment</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            {
-                                students?.map(student => <Student
-                                    id={student._id}
-                                    student={student}
-                                    handleStudentDelete={handleStudentDelete}
-                                ></Student>)
-                            }
-                            <tfoot>
-                                <tr>
-                                    <th></th>
-                                    <th></th>
-                                    <th></th>
-                                    <th></th>
-                                </tr>
-                            </tfoot>
-                        </table>
-                    </div>
-                </div>
-                <Sidebars></Sidebars>
-            </div>
-        </>
-    );
-}
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          refetch();
+        }
+      });
+  };
+  return (
+    <>
+      <DashboardNavbar />
+      <div className="drawer drawer-mobile">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content ">
+          <div className="overflow-x-auto w-full">
+            <table className="table w-full">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Address</th>
+                  <th>Depertment</th>
+                  <th></th>
+                </tr>
+              </thead>
+              {students?.map((student) => (
+                <Student
+                  id={student._id}
+                  student={student}
+                  handleStudentDelete={handleStudentDelete}
+                ></Student>
+              ))}
+              <tfoot>
+                <tr>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        </div>
+        <Sidebars></Sidebars>
+      </div>
+    </>
+  );
+};
 
 export default index;
-
-
-
--- pages/dashboard/students/pending/index.js
+++ pages/dashboard/students/pending/index.js
@@ -1,92 +1,91 @@
-import { useQuery } from "@tanstack/react-query"
-import DashboardNavbar from "../../../../components/Navbars/DashboardNavbar"
-import Pending from "../../../../components/Shared/Pending/pending"
-import Sidebars from "../../../../components/Sidebars/Sidebars"
-import Spiner from "../../../../components/Spiner/Spiner"
-import AlertMessage from "../../../../Hooks/AlertMessage"
-
-
-
+import { useQuery } from "@tanstack/react-query";
+import DashboardNavbar from "../../../../components/Navbars/DashboardNavbar";
+import Pending from "../../../../components/Shared/Pending/pending";
+import Sidebars from "../../../../components/Sidebars/Sidebars";
+import Spiner from "../../../../components/Spiner/Spiner";
+import AlertMessage from "../../../../Hooks/AlertMessage";
 
 const index = () => {
-    const { successMessage } = AlertMessage();
-    const url = `http://localhost:3100/users`
-    const { data: students = [], refetch, isLoading } = useQuery({
-        queryKey: [],
-        queryFn: async () => {
-            const res = await fetch(url)
-            const data1 = await res.json()
-            const data = data1.filter(ps => !ps.verification && ps.roll === "student")
-            return data;
-        }
+  const { successMessage } = AlertMessage();
+  const url = `http://localhost:3100/users`;
+  const {
+    data: students = [],
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: [],
+    queryFn: async () => {
+      const res = await fetch(url);
+      const data1 = await res.json();
+      const data = data1.filter(
+        (ps) => !ps.verification && ps.roll === "student"
+      );
+      return data;
+    },
+  });
+  console.log("Stu", students);
+
+  if (isLoading) {
+    return <Spiner></Spiner>;
+  }
+
+  const handleVerification = (student) => {
+    console.log("Deleting  with id: ", student);
+
+    fetch(`http://localhost:3100/users/${student._id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(),
     })
-    console.log("Stu", students)
-
-    if (isLoading) {
-        return <Spiner></Spiner>
-    }
-
-    const handleVerification = (student) => {
-        console.log('Deleting  with id: ', student)
-
-        fetch(`http://localhost:3100/users/${student._id}`, {
-            method: 'PATCH',
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: JSON.stringify()
-        })
-            .then(res => res.json())
-            .then(result => {
-                console.log("resultInside", result);
-                if (result.acknowledged === true) {
-                    successMessage("Approved")
-                    refetch()
-                }
-            })
-
-    }
-    return (
-        <>
-            <DashboardNavbar />
-            <div className="drawer drawer-mobile">
-                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content ">
-                    <div className="overflow-x-auto w-full">
-                        <table className="table w-full">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Address</th>
-                                    <th>Depertment</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            {
-                                students?.map(student => <Pending
-                                    id={student._id}
-                                    student={student}
-                                    handleVerification={handleVerification}
-                                ></Pending>)
-                            }
-                            <tfoot>
-                                <tr>
-                                    <th></th>
-                                    <th></th>
-                                    <th></th>
-                                    <th></th>
-                                </tr>
-                            </tfoot>
-                        </table>
-                    </div>
-                </div>
-                <Sidebars></Sidebars>
-            </div>
-        </>
-    );
-}
+      .then((res) => res.json())
+      .then((result) => {
+        console.log("resultInside", result);
+        if (result.acknowledged === true) {
+          successMessage("Approved");
+          refetch();
+        }
+      });
+  };
+  return (
+    <>
+      <DashboardNavbar />
+      <div className="drawer drawer-mobile">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content ">
+          <div className="overflow-x-auto w-full">
+            <table className="table w-full">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Address</th>
+                  <th>Depertment</th>
+                  <th></th>
+                </tr>
+              </thead>
+              {students?.map((student) => (
+                <Pending
+                  id={student._id}
+                  student={student}
+                  handleVerification={handleVerification}
+                ></Pending>
+              ))}
+              <tfoot>
+                <tr>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        </div>
+        <Sidebars></Sidebars>
+      </div>
+    </>
+  );
+};
 
 export default index;
-
-
-
