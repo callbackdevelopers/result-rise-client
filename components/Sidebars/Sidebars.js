9c13c0955366f6d4581bb7733bbb314162000c44
itzshakeeb <itzshakeeb@gmail.com>
2023-02-18 10:14:42 +0600

    starting layout upgrade

--- components/Sidebars/Sidebars.js
+++ components/Sidebars/Sidebars.js
@@ -4,15 +4,13 @@ const Sidebars = () => {
     return (
         <div className="drawer-side">
             <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-            <ul className="menu p-8 w-80 bg-base-100 lg:bg-inherit text-base-content">
+            <div className="menu py-4  w-64 bg-base-100 lg:bg-inherit text-base-content">
                 <li><Link href="/dashboard/profile">Profile</Link></li>
                 <li><Link href="/dashboard/result">Results</Link></li>
-                <hr />
                 <li><Link href="/dashboard/students/Report">Report Student</Link></li>
                 <li><Link href="/dashboard/students/pending/reporterdStudent">Pending Reported Students</Link></li>
                 <li><Link href="/dashboard/teachers/myreports">My Reports</Link></li>
                 <li><Link href="/dashboard/students/ReportedStudents">Reported Students</Link></li>
-                <hr />
                 <li><Link href="/dashboard/teachers">Teachers</Link></li>
                 <li><Link href="/dashboard/students">Students</Link></li>
                 <li><Link href="/dashboard/addmark">Add Marks</Link></li>
@@ -21,9 +19,7 @@ const Sidebars = () => {
                 <li><Link href="/dashboard/students/pending">Pending Students</Link></li>
                 <li><Link href="/dashboard/notification/shownotification">Notification</Link></li>
                 <li><Link href="/dashboard/notification/pushnotification">Notice Board</Link></li>
-
-                <hr />
-            </ul>
+            </div>
         </div>
     );
 }
