75ffeff6f6ae7cb5aefd2dbc0eecf2494189e0cc
itzshakeeb <itzshakeeb@gmail.com>
2023-02-24 00:43:51 +0600

    add a table

--- components/modals/AddNumber/stdentaddmarks.js
+++ components/modals/AddNumber/stdentaddmarks.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
-const Stdentaddmarks = ({ data }) => {
+const Stdentaddmarks = ({ data, showTable }) => {
   const [studentId, setStudentId] = useState(null);
+
   const handleAddFormChange = (event) => {
     event.preventDefault();
     const form = event.target;
@@ -27,64 +28,65 @@ const Stdentaddmarks = ({ data }) => {
       );
   };
   return (
-    <section>
-      {/* add marks section */}
-      <div className="border bg-white ">
-        <ul className="gap-1 border bg-gray-200 
+    <section >
+
+      {showTable &&
+        <div className="border bg-white ">
+          <ul className="gap-1 border bg-gray-200 
         form-control grid grid-cols-7  md:gap-3  items-center px-5 py-2">
-          <li> SL </li>
-          <li> Name </li>
-          <li> Presentation  </li>
-          <li> Midterm  </li>
-          <li> Final  </li>
-          <li> Viva  </li>
-          <li> Status </li>
-        </ul>
-        <div>
-          {data?.map((examResult) => (
-            <>
-              <form
-                onSubmit={handleAddFormChange}
-                key={examResult._id}
-                className=" form-control grid grid-cols-7  md:gap-3 gap-1 items-center my-2">
-                <span className="mx-2 w-10">{examResult.id}</span>
-                <label className=" ">
-                  {examResult.name}
-                </label>
-                <input
-                  type="number"
-                  className="input input-bordered  max-w-xs input-sm w-full "
-                  placeholder="presenatation marks"
-                  name="presentation_score"
-                ></input>
-                <input
-                  type="number"
-                  className="input input-bordered input-sm w-full max-w-xs"
-                  placeholder="midterm marks"
-                  name="midterm_score"
-                ></input>
-                <input
-                  type="number"
-                  className="input input-bordered input-sm w-full max-w-xs"
-                  placeholder="final marks"
-                  name="final_exam_score"
-                ></input>
-                <input
-                  type="number"
-                  className="input input-bordered input-sm w-full max-w-xs"
-                  placeholder="viva marks"
-                  name="viva_score"
-                ></input>
-                <button
-                  onClick={() => setStudentId(examResult.id)}
-                  className="mx-2 btn btn-xs " type="submit">
-                  Add
-                </button>
-              </form>
-            </>
-          ))}
-        </div>
-      </div>
+            <li> SL </li>
+            <li> Name </li>
+            <li> Presentation  </li>
+            <li> Midterm  </li>
+            <li> Final  </li>
+            <li> Viva  </li>
+            <li> Status </li>
+          </ul>
+          <div>
+            {data?.map((examResult) => (
+              <>
+                <form
+                  onSubmit={handleAddFormChange}
+                  key={examResult._id}
+                  className=" form-control grid grid-cols-7  md:gap-3 gap-1 items-center my-2">
+                  <span className="mx-2 w-10">{examResult.id}</span>
+                  <label className=" ">
+                    {examResult.name}
+                  </label>
+                  <input
+                    type="number"
+                    className="input input-bordered  max-w-xs input-sm w-full "
+                    placeholder="presenatation marks"
+                    name="presentation_score"
+                  ></input>
+                  <input
+                    type="number"
+                    className="input input-bordered input-sm w-full max-w-xs"
+                    placeholder="midterm marks"
+                    name="midterm_score"
+                  ></input>
+                  <input
+                    type="number"
+                    className="input input-bordered input-sm w-full max-w-xs"
+                    placeholder="final marks"
+                    name="final_exam_score"
+                  ></input>
+                  <input
+                    type="number"
+                    className="input input-bordered input-sm w-full max-w-xs"
+                    placeholder="viva marks"
+                    name="viva_score"
+                  ></input>
+                  <button
+                    onClick={() => setStudentId(examResult.id)}
+                    className="mx-2 btn btn-xs " type="submit">
+                    Add
+                  </button>
+                </form>
+              </>
+            ))}
+          </div>
+        </div>}
     </section>
   );
 };
--- pages/dashboard/addmark/index.js
+++ pages/dashboard/addmark/index.js
@@ -6,6 +6,7 @@ import Layout from "../../../Layout/Layout";
 
 function index() {
   const [show, setShow] = useState(false);
+  const [showTable, setShowTable] = useState(false);
   const [student, setStudent] = useState([]);
   const url = `http://localhost:3100/verified/student`;
   const {
@@ -27,22 +28,61 @@ function index() {
     <div>
       <div>
         <Layout>
+
           <div className='bg-gray-100 min-h-screen'>
+            {!showTable &&
+              <div className="flex items-center h-[80vh] justify-center">
+                <div className="border lg:w-2/3 mx-auto bg-white shadow-sm p-4 md:p-7">
+                  <div className="text-2xl md:text-3xl text-center">Please Provide The information</div>
+                  <div className="flex flex-col items-center justify-center mt-5 p-4 md:p-0">
+                    <div className="w-4/5 flex flex-col items-center py-5">
+                      <div className="flex items-center w-full md:w-2/3 py-2">
+                        <label className="mx-2 w-full">Select Course</label>
+                        <select className="input input-bordered input-sm w-full max-w-xs">
+                          <option value="1">CSE 101</option>
+                          <option value="2">CSE 102</option>
+                          <option value="3">CSE 103</option>
+                          <option value="4">CSE 104</option>
+                          <option value="5">CSE 105</option>
+                        </select>
+                      </div>
+                      <div className="flex items-center w-full md:w-2/3 mt-2">
+                        <label className="mx-2 w-full">Select Semester</label>
+                        <select className="input input-bordered input-sm w-full max-w-xs">
+                          <option value="1">1st</option>
+                          <option value="2">2nd</option>
+                          <option value="3">3rd</option>
+                          <option value="4">4th</option>
+                          <option value="5">5th</option>
+                          <option value="6">6th</option>
+                          <option value="7">7th</option>
+                          <option value="8">8th</option>
+                        </select>
+                      </div>
+                      <div className="flex items-center w-full md:w-2/3 mt-2">
+                        <label className="mx-2 w-full">Select Section</label>
+                        <select className="input input-bordered input-sm w-full max-w-xs">
+                          <option value="1">A</option>
+                          <option value="2">B</option>
+                          <option value="3">C</option>
+                          <option value="4">D</option>
+                          <option value="5">E</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className=" w-full md:w-3/5 text-right">
+                      <button
+                        type="submit"
+                        className="btn btn-sm mt-5">Get Students</button>
+                    </div>
+                  </div>
+                </div>
+              </div>}
             <div className="p-6">
               <Stdentaddmarks
                 data={students}
+                showTable={showTable}
               />
-            </div>
-            <div className="m-4">
-              <label
-                onClick={() => {
-                  setStudent(students), setShow(true);
-                }}
-                htmlFor="add_Number_modal"
-                className="btn btn-sm btn-warning"
-              >
-                View Marks
-              </label>
             </div>
           </div>
         </Layout>
